e0370c1faf176965255ebdf8f67ce16de70534da
Anush Naik <aanushnaik@gmail.com>
2022-08-30 11:50:11 +0530

    heroku app

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1 className={styles.h1}>HELLO ALL!</h1> 
+    </div>
+  )
+}
+
+export default Home
